14a5a64b3cf99ad6ef724553c5332da1d7cce8b0
TheRealIanGray <therealiangray@gmail.com>
2021-03-08 08:42:18 -0600

    through q9

--- data/baseball_sql.sql
+++ data/baseball_sql.sql
@@ -3,6 +3,8 @@
 from batting*/
 --1817-2016
 
+
+
 /*Question 2- Find the name and height of the shortest player in the database. 
 			How many games did he play in? 
 			What is the name of the team for which he played?*/
@@ -15,6 +17,8 @@ left join teams as t
 on t.teamid=b.teamid
 where p.playerid='gaedeed01'
 order by height asc*/
+
+
 
 /*Question 3- Find all players in the database who played at Vanderbilt University. 
 				Create a list showing each player’s first and last names as well as the total salary 
@@ -37,52 +41,178 @@ inner join people as p
 on vandy_players.playerid = p.playerid
 order by sal.salaryx desc*/
 
+
+
 /*Question 4- Using the fielding table, group players into three groups based on their position: 
 				label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", 
 				and those with position "P" or "C" as "Battery". 
 				Determine the number of putouts made by each of these three groups in 2016.*/
 				
-select playerid, case when pos = 'OF' then 'Outfield'
-						/*when pos = ('SS', '1B', '2B', '3B') then 'Infield'*/
+/*select sum(PO), case when pos = 'OF' then 'Outfield'
+						when pos in ('SS', '1B', '2B', '3B') then 'Infield'
 						when pos = 'P' then 'Battery' 
-						when pos = 'C' then 'Battery' else 'na' end as 'Position',
-						
-		year
+						when pos = 'C' then 'Battery' else 'na' end as Position
 from fielding
-order by p
-				
+where yearid = '2016'
+group by Position
+order by Position*/
+		
+		
+
 /*Question 5- Find the average number of strikeouts per game by decade since 1920. 
 				Round the numbers you report to 2 decimal places. 
 				Do the same for home runs per game. 
 				Do you see any trends?*/
-				
-select round(avg(soa), 2), left(cast(yearid as varchar(4)), 3) as decade
-from teams
-group by decade
-order by decade
 
-select *
-from teams
-				
+/*select round(sum(cast(soa as decimal)) / sum(cast(g as decimal)), 2) as avg_strikout_by_game, 
+				round(sum(cast(hr as decimal)) / sum(cast(g as decimal)), 2) as avg_homerun_by_game,
+				left(cast(yearid as varchar(4)), 3) || '0s' as decade
+from teams as t
+group by decade
+order by decade*/
+
+
+	
 /*Question 6- Find the player who had the most success stealing bases in 2016, 
 				where success is measured as the percentage of stolen base attempts which are successful. 
 				(A stolen base attempt results either in a stolen base or being caught stealing.) 
-				Consider only players who attempted at least 20 stolen bases.
+				Consider only players who attempted at least 20 stolen bases.*/
 				
-/*Question 7- From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? 
-				What is the smallest number of wins for a team that did win the world series? 
-				Doing this will probably result in an unusually small number of wins for a world series champion 
-				– determine why this is the case. 
-				Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that 
+/*select playerid, round((cast(sb as decimal)/(cast(sb as decimal) + cast(cs as decimal))),2) as stolen
+from batting
+where yearid = '2016'
+		and (sb + cs) > 19
+order by stolen desc*/
+
+				
+				
+/*Question 7- From 1970 – 2016, what is the largest number of wins for a team that did not win the world series?*/
+				
+/*select teamid, yearid, w, WSWin
+from teams
+where yearid > 1969
+and wswin = 'N'
+order by w desc
+limit 1*/
+
+/*What is the smallest number of wins for a team that did win the world series?*/
+
+/*select teamid, yearid, w, WSWin
+from teams
+where yearid > 1969
+and wswin = 'Y'
+order by w asc
+limit 1*/
+
+/*Doing this will probably result in an unusually small number of wins for a world series champion 
+				– determine why this is the case.*/
+
+/*select max(w), min(w), yearid, WSWin
+from teams
+group by yearid, wswin
+order by yearid desc*/
+
+/*Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that 
 				a team with the most wins also won the world series? What percentage of the time?*/
+
+/*with w as (select yearid, max(w) as ww, WSWin
+		from teams
+		where yearid > 1969
+		and yearid != 1981
+		and wswin = 'Y'
+		group by yearid, wswin
+		order by yearid desc),
+	l as (select yearid, max(w) as lw, WSWin
+		from teams
+		where yearid > 1969
+		and yearid != 1981
+		and wswin = 'N'
+		group by yearid, wswin
+		order by yearid desc)
+select round(sum(case when ww > lw then cast('1.0' as decimal)
+			when ww <= lw then cast('0.0' as decimal) end)/
+			count(distinct w.yearid), 2)
+from w
+join l
+on w.yearid = l.yearid*/
 				
 /*Question 8- Using the attendance figures from the homegames table, find the teams and parks which had the 
 				top 5 average attendance per game in 2016 
 				(where average attendance is defined as total attendance divided by number of games). 
 				Only consider parks where there were at least 10 games played. 
 				Report the park name, team name, and average attendance. 
-				Repeat for the lowest 5 average attendance.
-				
+				Repeat for the lowest 5 average attendance.*/
+
+/*Parks top 5 attendance*/
+/*with p as (select park_name, park
+			from parks)
+select p.park_name, sum(attendance)/sum(games) as att_ratio
+from homegames as hg
+join p 
+on hg.park = p.park
+where year = 2016
+group by p.park_name
+having sum(games) > 9
+order by att_ratio desc
+limit 5*/
+
+/*Teams top 5 attendance*/
+/*with t as (select teamid, name
+			 from teams)
+select name, sum(attendance)/sum(games) as att_ratio
+from homegames as hg
+join t 
+on hg.team = t.teamid
+where year = 2016
+group by name
+order by att_ratio desc
+limit 5*/
+
+
+
 /*Question 9- Which managers have won the TSN Manager of the Year award in both 
 				the National League (NL) and the American League (AL)? 
-				Give their full name and the teams that they were managing when they won the award.
+				Give their full name and the teams that they were managing when they won the award.*/
+
+--had to include duplicated/reversed CTE to get distinct year values for when the two awards were won each year
+--instead of just a single year, which would have meant only finding one of the teams coached at the time.
+
+/*with nl as (select playerid, yearid
+from awardsmanagers
+where awardid = 'TSN Manager of the Year'
+and lgid = 'NL'
+and playerid in (select playerid
+				from awardsmanagers
+				where awardid = 'TSN Manager of the Year'
+				and lgid = 'AL')),
+	al as (select playerid, yearid
+			from awardsmanagers
+			where awardid = 'TSN Manager of the Year'
+			and lgid = 'AL'
+			and playerid in (select playerid
+				from awardsmanagers
+				where awardid = 'TSN Manager of the Year'
+				and lgid = 'NL'))
+select namelast, namefirst, m.yearid, name
+from nl
+join people as p
+on nl.playerid = p.playerid
+join managers as m
+on m.playerid = p.playerid
+and m.yearid = nl.yearid
+join teams as t
+on m.teamid = t.teamid
+and m.yearid = t.yearid
+union all
+select namelast, namefirst, m.yearid, name
+from al
+join people as p
+on al.playerid = p.playerid
+join managers as m
+on m.playerid = p.playerid
+and m.yearid = al.yearid
+join teams as t
+on m.teamid = t.teamid
+and m.yearid = t.yearid
+order by yearid desc*/
+
